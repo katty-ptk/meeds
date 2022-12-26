27b3ceabe8db0cd7af2913ab629773a0643766b1
Katty Ptk <46168818+katty-ptk@users.noreply.github.com>
2022-12-26 08:59:35 +0200

    landing page (#2)
    * landing page
    finished landing page
     > added buttons for login and sign up
     > added Montserrat font
     > implemented navigation between landing page and sign up page
     > made signUp and login buttons same width
     > added google icon for google authentication

---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import './signup_page.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Color green = Colors.green.shade600;
+    Color black = Colors.black87;
+
+    return Scaffold(
+      body: Flex(
+        direction: Axis.vertical,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(width: double.infinity,),
+          Column(
+            children: [
+              RichText(
+                text: TextSpan(
+                  text: 'Welcome to ',
+                  style: const TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'meeds', 
+                      style: TextStyle(
+                          color: green,
+                          fontStyle: FontStyle.italic
+                      )
+                    ),
+                    const TextSpan(text: '!',)
+                  ],
+                ),
+              ),
+              Image.asset(
+                'assets/images/logo.jpg',
+                scale: 10,
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: (){
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => const SignUpPage()));
+                    }, 
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: black,
+                      minimumSize: const Size(100, 30),
+                    ),
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Montserrat-Bold'
+                      ),
+                    )
+                  ),
+                  ElevatedButton(
+                    onPressed: (){}, 
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: black,
+                      minimumSize: const Size(100, 30),
+                    ),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Montserrat-Bold'
+                      ),
+                    )
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text('- or -',),
+                  ),
+                  GestureDetector(
+                    onTap: (){
+                     
+                    },
+                    child: Image.asset(
+                      'assets/images/google.png', 
+                      height: 32,
+                    )
+                  )
+                ],
+              ),
+            ],
+          ),
+          Text(
+            'Want to learn more?',
+            style: TextStyle(
+              decoration: TextDecoration.underline,
+              color: green,
+              fontFamily: 'Montserrat-Bold',
+              letterSpacing: 0.7
+            ),
+          )
+        ],
+      )
+    );
+  }
+}
