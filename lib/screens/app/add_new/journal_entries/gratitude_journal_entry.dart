9192e9508aa16a083508ae826dea61bd0eda8bc2
Katty Ptk <kattyptk@Kattys-MacBook-Pro.local>
2023-01-24 11:16:12 +0200

    made UI for gratitude journal entry

--- lib/screens/app/add_new/journal_entries/gratitude_journal_entry.dart
+++ lib/screens/app/add_new/journal_entries/gratitude_journal_entry.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meeds/utils/meeds_colors.dart';
 import 'package:meeds/widgets/entries/journal_entry.dart';
 import 'package:meeds/widgets/profile_image.dart';
 
@@ -13,13 +14,103 @@ class _GratitudeJournalEntryState extends State<GratitudeJournalEntry> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        ProfileImageBG(non_profile: true, text: "Gratitude",),
-        Padding(
-          padding: const EdgeInsets.all(35),
-          child: JournalEntry(data: "sad", date: "01.01.0101", type: "gratitude"),
-        )
-      ],)
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          ProfileImageBG(non_profile: true, text: "Gratitude",),
+          SizedBox(height: 40,),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Text(
+              '“When we focus on our gratitude, the tide of disappointment goes out and the tide of love rushes in.“',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.grey.shade500,
+                fontStyle: FontStyle.italic
+              ),
+            ),
+          ),
+
+          SizedBox(height: 50,),
+
+          // form
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(children: [
+              ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxHeight: 200
+                ),
+                child: TextFormField(
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: MyColors.light_pink,
+                        width: 2
+                      )
+                    ),
+                    label: Text("Why are you grateful for this?"),
+                    labelStyle: TextStyle(
+                      color: MyColors.dark_purple
+                    ),
+                    hintText: "😉: Because it helpes me ...",
+                  ),
+                ),
+              ),
+              SizedBox(height: 30,),
+
+              ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxHeight: 200
+                ),
+                child: TextFormField(
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: MyColors.light_pink,
+                        width: 2
+                      )
+                    ),
+                    label: Text("Why are you grateful for this?"),
+                    labelStyle: TextStyle(
+                      color: MyColors.dark_purple
+                    ),
+                    hintText: "😉: Because it helpes me ...",
+                  ),
+                ),
+              ),
+              SizedBox(height: 30,),
+
+              ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxHeight: 200
+                ),
+                child: TextFormField(
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: MyColors.light_pink,
+                        width: 2
+                      )
+                    ),
+                    label: Text("Why are you grateful for this?"),
+                    labelStyle: TextStyle(
+                      color: MyColors.dark_purple
+                    ),
+                    hintText: "😉: Because it helpes me ...",
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 50,),
+              Icon(Icons.check_circle, color: Colors.green, size: 48,),
+              SizedBox(height: 150,)
+            ],),
+          )
+        ]),
     );
   }
 }
